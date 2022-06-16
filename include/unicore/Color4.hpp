668a1aa8afa6ba6bfc76239e1cc3192c68195fa9
Denis <kolyden@gmail.com>
2022-06-16 11:52:07 +0200

    Update Color4

--- include/unicore/Color4.hpp
+++ include/unicore/Color4.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "unicore/Random.hpp"
+#include "unicore/PixelFormat.hpp"
 
 namespace unicore
 {
@@ -20,8 +21,9 @@ namespace unicore
 	};
 
 	template<typename T>
-	struct Color4
+	class Color4
 	{
+	public:
 		T r, g, b, a;
 
 		static constexpr T MinValue = color_limits<T>::min();
@@ -33,17 +35,36 @@ namespace unicore
 		constexpr Color4(T _r, T _g, T _b, T _a)
 			: r(_r), g(_g), b(_b), a(_a) {}
 
-		static constexpr T convert_component_from_uint8(uint8_t value)
+		template<
+			typename DataType,
+			typename ComponentType>
+		static constexpr Color4 from_format(PixelFormat<DataType, ComponentType> format, DataType value)
 		{
-			if (std::is_same_v<T, uint8_t>) return value;
-
-			if (std::is_floating_point_v<T>)
-				return static_cast<T>((static_cast<T>(value) / 255.f) * color_limits<T>::max());
-
-			return static_cast<T>((static_cast<double>(value) / 255.0f) * color_limits<T>::range()) + color_limits<T>::min();
+			const auto r = static_cast<uint8_t>(value >> format.r_shift) & format.component_mask;
+			const auto g = static_cast<uint8_t>(value >> format.g_shift) & format.component_mask;
+			const auto b = static_cast<uint8_t>(value >> format.b_shift) & format.component_mask;
+			const auto a = static_cast<uint8_t>(value >> format.a_shift) & format.component_mask;
+			return {
+				convert_component_from_uint8(r),
+				convert_component_from_uint8(g),
+				convert_component_from_uint8(b),
+				convert_component_from_uint8(a),
+			};
 		}
 
-		static constexpr Color4 from_rgb(uint32_t rgb)
+		template<
+			typename DataType,
+			typename ComponentType>
+		constexpr DataType to_format(PixelFormat<DataType, ComponentType> format) const
+		{
+			return
+				(convert_component_to_uint8(r) << format.r_shift) |
+				(convert_component_to_uint8(g) << format.g_shift) |
+				(convert_component_to_uint8(b) << format.b_shift) |
+				(convert_component_to_uint8(a) << format.a_shift);
+		}
+
+		static constexpr Color4 from_rgb(const uint32_t rgb)
 		{
 			const uint8_t r = (rgb >> 16) & 0xFF;
 			const uint8_t g = (rgb >> 8) & 0xFF;
@@ -56,19 +77,22 @@ namespace unicore
 			};
 		}
 
-		template<typename U = T, std::enable_if_t<std::is_integral_v<U>>* = nullptr>
-		static constexpr Color4 from_argb(uint32_t rgba)
+		[[nodiscard]] constexpr uint32_t to_rgb() const
 		{
-			const uint8_t a = (rgba >> 24) & 0xFF;
-			const uint8_t r = (rgba >> 16) & 0xFF;
-			const uint8_t g = (rgba >> 8) & 0xFF;
-			const uint8_t b = rgba & 0xFF;
-			return {
-				convert_component_from_uint8(r),
-				convert_component_from_uint8(g),
-				convert_component_from_uint8(b),
-				convert_component_from_uint8(a),
-			};
+			return
+				(convert_component_to_uint8(r) << 16) |
+				(convert_component_to_uint8(g) << 8) |
+				convert_component_to_uint8(b);
+		}
+
+		static constexpr Color4 from_argb(const uint32_t argb)
+		{
+			return from_format(pixel_format_argb, argb);
+		}
+
+		static constexpr Color4 from_rgba(const uint32_t rgba)
+		{
+			return from_format(pixel_format_rgba, rgba);
 		}
 
 		static Color4 create_random(Random& random)
@@ -78,6 +102,27 @@ namespace unicore
 				random.range(MinValue, MaxValue),
 				random.range(MinValue, MaxValue),
 			};
+		}
+
+	protected:
+		static constexpr T convert_component_from_uint8(uint8_t value)
+		{
+			if (std::is_same_v<T, uint8_t>) return value;
+
+			if (std::is_floating_point_v<T>)
+				return static_cast<T>((static_cast<T>(value) / 255.0f) * color_limits<T>::max());
+
+			return static_cast<T>((static_cast<double>(value) / 255.0) * color_limits<T>::range()) + color_limits<T>::min();
+		}
+
+		[[nodiscard]] constexpr uint8_t convert_component_to_uint8(T value) const
+		{
+			if (std::is_same_v<T, uint8_t>) return value;
+
+			if (std::is_floating_point_v<T>)
+				return static_cast<uint8_t>((value / color_limits<T>::max()) * 0xFF);
+
+			return static_cast<uint8_t>((static_cast<double>(value) / static_cast<double>(color_limits<T>::range())) * 0xFF);
 		}
 	};
 
---
+++ include/unicore/PixelFormat.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "unicore/Defs.hpp"
+
+namespace unicore
+{
+	template<
+		typename DataType,
+		typename ComponentType>
+	struct PixelFormat
+	{
+		using Data = DataType;
+		using Element = ComponentType;
+
+		static constexpr auto component_mask = std::numeric_limits<ComponentType>::max();
+
+		uint8_t r_shift, g_shift, b_shift, a_shift;
+	};
+
+	static constexpr PixelFormat<uint32_t, uint8_t> pixel_format_argb{ 16, 8, 0, 24 };
+	static constexpr PixelFormat<uint32_t, uint8_t> pixel_format_rgba{ 24, 16, 8, 0 };
+}
