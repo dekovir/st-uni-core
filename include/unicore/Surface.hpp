c20290e5a4d383bb59947eb290e468a16c78ad8d
Denis <kolyden@gmail.com>
2022-07-13 11:21:55 +0200

    Update

--- include/unicore/Surface.hpp
+++ include/unicore/Surface.hpp
@@ -6,6 +6,7 @@
 
 namespace unicore
 {
+	// TODO: Rename to something else (Bitmap, PixelBuffer, ColorBuffer)
 	class Surface : public Resource, public IBuffer2<Color4b>
 	{
 		UC_OBJECT(Surface, Resource)
