b03a88ca4aadbbd79f087e4217abf01e93a25458
Denis <kolyden@gmail.com>
2022-09-13 09:19:12 +0200

    Update

--- include/unicore/MemoryData.hpp
+++ include/unicore/MemoryData.hpp
@@ -18,9 +18,7 @@ namespace unicore
 	class ConstMemoryData : public MemoryData
 	{
 	public:
-		ConstMemoryData(const void* data, const size_t size)
-			: _data(data), _size(size)
-		{}
+		ConstMemoryData(const void* data, const size_t size);
 
 		UC_NODISCARD size_t size() const override { return _size; }
 		UC_NODISCARD const void* data() const override { return _data; }
@@ -33,31 +31,18 @@ namespace unicore
 	class DynamicMemoryData : public MemoryData
 	{
 	public:
-		explicit DynamicMemoryData(size_t size)
-			: _data(size)
-		{}
+		explicit DynamicMemoryData(size_t size);
 
-		DynamicMemoryData(size_t size, uint8_t value)
-			: _data(size, value) {}
+		DynamicMemoryData(size_t size, uint8_t value);
 
 		UC_NODISCARD size_t size() const override { return _data.size(); }
 		UC_NODISCARD const void* data() const override { return _data.data(); }
 		UC_NODISCARD void* data() { return _data.data(); }
 
-		void resize(size_t new_size)
-		{
-			_data.resize(new_size);
-		}
+		void resize(size_t new_size);
+		void resize(size_t new_size, uint8_t value);
 
-		void resize(size_t new_size, uint8_t value)
-		{
-			_data.resize(new_size, value);
-		}
-
-		void fill(uint8_t value = 0)
-		{
-			std::fill(_data.begin(), _data.end(), value);
-		}
+		void fill(uint8_t value = 0);
 
 	protected:
 		List<uint8_t> _data;
