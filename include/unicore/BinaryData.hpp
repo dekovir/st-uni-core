bae94b512fd3c17288a4b771e605b9231cfa67bf
Denis <kolyden@gmail.com>
2022-10-03 15:39:05 +0200

    Update

--- include/unicore/BinaryData.hpp
+++ include/unicore/BinaryData.hpp
@@ -4,18 +4,45 @@
 
 namespace unicore
 {
-	class BinaryData : public Resource
+	class BasicBinaryData : public Resource
 	{
-		UC_OBJECT(BinaryData, Resource)
+		UC_OBJECT(BasicBinaryData, Resource)
+	public:
+		UC_NODISCARD virtual const Byte* data() const = 0;
+		UC_NODISCARD virtual size_t size() const = 0;
+		// TODO: Return span
+	};
+
+	class BinaryData : public BasicBinaryData
+	{
+		UC_OBJECT(BinaryData, BasicBinaryData)
 	public:
 		explicit BinaryData(MemoryChunk chunk);
 
 		UC_NODISCARD size_t get_system_memory_use() const override { return sizeof(BinaryData) + _chunk.size(); }
 
-		UC_NODISCARD const void* data() const { return _chunk.data(); }
-		UC_NODISCARD size_t size() const { return _chunk.size(); }
+		UC_NODISCARD const Byte* data() const override { return static_cast<const Byte*>(_chunk.data()); }
+		UC_NODISCARD size_t size() const override { return _chunk.size(); }
 
 	protected:
 		MemoryChunk _chunk;
+	};
+
+	class DynamicBinaryData : public BasicBinaryData
+	{
+		UC_OBJECT(DynamicBinaryData, BasicBinaryData)
+	public:
+		explicit DynamicBinaryData(size_t size) : _data(size) {}
+		DynamicBinaryData(size_t size, Byte default_value) : _data(size, default_value) {}
+
+		UC_NODISCARD size_t get_system_memory_use() const override { return sizeof(DynamicBinaryData) + _data.size(); }
+
+		UC_NODISCARD const Byte* data() const override { return _data.data(); }
+		UC_NODISCARD size_t size() const override { return _data.size(); }
+
+		UC_NODISCARD Byte* data() { return _data.data(); }
+
+	protected:
+		List<Byte> _data;
 	};
 }
