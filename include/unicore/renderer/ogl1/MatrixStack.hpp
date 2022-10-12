c13bef8e9c34830192ef34b1402308ed03da5cf8
Denis <kolyden@gmail.com>
2022-10-12 13:34:47 +0200

    Update

--- include/unicore/renderer/ogl1/MatrixStack.hpp
+++ include/unicore/renderer/ogl1/MatrixStack.hpp
@@ -33,15 +33,23 @@ namespace unicore::ogl1
 		virtual void push_matrix() = 0;
 		virtual void pop_matrix() = 0;
 
-		virtual void translate(Float x, Float y, Float z) { mult_matrix(Matrix4f::translate(x, y, z)); }
-		virtual void translate(const Vector3f& vec) { translate(vec.x, vec.y, vec.z); }
+		virtual void translate3f(Float x, Float y, Float z) { mult_matrix(Matrix4f::translate(x, y, z)); }
+		virtual void translate3f(const Vector3f& vec) { translate3f(vec.x, vec.y, vec.z); }
+		// TODO: Replace with span
+		virtual void translate3fv(const Float* v) { translate3f(v[0], v[1], v[2]); }
 
-		virtual void rotate_x(Radians angle) { mult_matrix(Matrix4f::rotate_x(angle)); }
-		virtual void rotate_y(Radians angle) { mult_matrix(Matrix4f::rotate_y(angle)); }
-		virtual void rotate_z(Radians angle) { mult_matrix(Matrix4f::rotate_z(angle)); }
-		//virtual void rotate(Radians x, Radians y, Radians z) = 0;
+		virtual void rotate_xf(Radians angle) { mult_matrix(Matrix4f::rotate_x(angle)); }
+		virtual void rotate_yf(Radians angle) { mult_matrix(Matrix4f::rotate_y(angle)); }
+		virtual void rotate_zf(Radians angle) { mult_matrix(Matrix4f::rotate_z(angle)); }
 
-		virtual void scale(Float x, Float y, Float z) { mult_matrix(Matrix4f::scale(x, y, z)); }
-		virtual void scale(const Vector3f& vec) { scale(vec.x, vec.y, vec.z); }
+		virtual void rotate3f(Radians x, Radians y, Radians z) = 0;
+		virtual void rotate3f(const Angles3f& angles) { rotate3f(angles.x, angles.y, angles.z); }
+		// TODO: Replace with span
+		virtual void rotate3fv(const Radians* v) { rotate3f(v[0], v[1], v[2]); }
+
+		virtual void scale3f(Float x, Float y, Float z) { mult_matrix(Matrix4f::scale(x, y, z)); }
+		virtual void scale3f(const Vector3f& vec) { scale3f(vec.x, vec.y, vec.z); }
+		// TODO: Replace with span
+		virtual void scale3fv(const Float* v) { scale3f(v[0], v[1], v[2]); }
 	};
 }
