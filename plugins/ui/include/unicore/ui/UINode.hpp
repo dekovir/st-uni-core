a2ebd3096c85298dec3fcd501a9978bb0088d02b
Denis <kolyden@gmail.com>
2022-11-03 15:32:06 +0100

    Update

--- plugins/ui/include/unicore/ui/UINode.hpp
+++ plugins/ui/include/unicore/ui/UINode.hpp
@@ -1,30 +1,9 @@
 #pragma once
-#include "UINode.hpp"
 #include "unicore/ui/UIAttribute.hpp"
+#include "unicore/ui/UIAction.hpp"
 
 namespace unicore
 {
-	// ACTIONS
-	class UINode;
-
-	using UIActionDefault = std::function<void()>;
-	using UIActionNodeDefault = std::function<void(const UINode&)>;
-
-	using UIActionValue = std::function<void(const Variant&)>;
-	using UIActionNodeValue = std::function<void(const UINode&, const Variant&)>;
-
-	using UIAction = StdVariant<UIActionDefault, UIActionNodeDefault, UIActionValue, UIActionNodeValue>;
-
-	enum class UIActionType : uint8_t
-	{
-		OnClick,
-		OnChange, // Value changed
-		OnMouseEnter, // Mouse cursor enters
-		OnMouseLeave, // Mouse cursor enters
-	};
-	using UIActionDict = Dictionary<UIActionType, UIAction>;
-
-	// NODE
 	enum class UINodeType
 	{
 		Group,
